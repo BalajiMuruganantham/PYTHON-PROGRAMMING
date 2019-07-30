db3e73af8c0fd8c1e228d4d743cc24f41a240783
BalajiMuruganantham <51204620+BalajiMuruganantham@users.noreply.github.com>
2019-07-30 12:06:40 +0530

    Update pro 25.py

--- pro 25.py
+++ pro 25.py
@@ -1,14 +1,14 @@
 def re(bal,sz):
     res=[]
     count=1
-    for i in range(0,size-1):
-        if arr[i]<arr[i+1]:
+    for i in range(0,sz-1):
+        if bal[i]<bal[i+1]:
             count+=1
         else:
             res.append(count)
             count=1
     res.append(count)
     print(max(res))
-size=int(input())
-arr=list(map(int,input().split()))
+sz=int(input())
+bal=list(map(int,input().split()))
 re(bal,sz)
